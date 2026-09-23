e64661078bc23fc880a7dd01d5273e56c063594b
DDoykov <82573519+DDoykov@users.noreply.github.com>
2026-09-23 17:14:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector('.menu-toggle').addEventListener('click',()=>document.querySelector('.nav-links').classList.toggle('open'));
+document.querySelectorAll('.nav-links a').forEach(a=>a.addEventListener('click',()=>document.querySelector('.nav-links').classList.remove('open')));
+document.getElementById('year').textContent=new Date().getFullYear();
